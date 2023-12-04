d7100d0e8a208065f4aaf026ed23114f8407d9c7
Nathaly Bataglia <nathalybataglia@outlook.com>
2023-12-04 11:41:35 -0300

    início firebase

--- app/src/main/java/br/edu/scl/ifsp/ads/contatospdm/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/contatospdm/view/MainActivity.kt
@@ -3,6 +3,9 @@ package br.edu.scl.ifsp.ads.contatospdm.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
@@ -20,6 +23,7 @@ import br.edu.scl.ifsp.ads.contatospdm.adapter.ContactAdapter
 import br.edu.scl.ifsp.ads.contatospdm.controller.ContactController
 import br.edu.scl.ifsp.ads.contatospdm.controller.ContactRoomController
 import br.edu.scl.ifsp.ads.contatospdm.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.ads.contatospdm.model.Constant.CONTACT_ARRAY
 import br.edu.scl.ifsp.ads.contatospdm.model.Contact
 import br.edu.scl.ifsp.ads.contatospdm.model.Constant.EXTRA_CONTACT
 import br.edu.scl.ifsp.ads.contatospdm.model.Constant.VIEW_CONTACT
@@ -43,6 +47,19 @@ class MainActivity : AppCompatActivity() {
             this,
             contactList
         )
+    }
+
+    val updateContactListHandler = object : Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message){
+            super.handleMessage(msg)
+            msg.data.getParcelableArray(CONTACT_ARRAY)?.also { contactArray ->
+                contactList.clear()
+                contactArray.forEach {
+                    contactList.add(it as Contact)
+                }
+                contactAdapter.notifyDataSetChanged()
+            }
+        }
     }
 
     // carl = ContactActivityResultLauncher
